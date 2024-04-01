68ae007fbf6abcea74763df43480af9674f5d81c
SMartin <santiagomartin.44@gmail.com>
2024-04-01 19:38:53 -0300

    minor changes

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,4 @@
-import dotenv from 'dotenv';
+/* import dotenv from 'dotenv';
 
 export const getVariables = (options) => {
     const environment = options.opts().mode;
@@ -15,4 +15,31 @@ export const getVariables = (options) => {
         githubClientId: process.env.GITHUB_CLIENT_ID,
         githubClientSecret: process.env.GITHUB_CLIENT_SECRET
     }
-}
+} */
+
+import dotenv from 'dotenv';
+import path from 'path';
+
+export const getVariables = (options) => {
+    // Obtener el entorno de ejecución especificado por el usuario o utilizar 'development' por defecto
+    const environment = options.opts().mode || process.env.NODE_ENV || 'development';
+    
+    // Establecer NODE_ENV para que otras partes de la aplicación puedan acceder a él si es necesario
+    process.env.NODE_ENV = environment;
+
+    // Determinar la ruta del archivo .env basándose en el entorno de ejecución
+    const envPath = path.resolve(process.cwd(), 'src', `.env.${environment}`);
+
+    // Cargar las variables de entorno desde el archivo .env correspondiente
+    dotenv.config({ path: envPath });
+    // Retornar las variables de entorno necesarias
+    return {
+        port: process.env.PORT,
+        mongoUrl: process.env.MONGO_URL,
+        secret: process.env.SECRET_KEY,
+        userAdmin: process.env.USERADMIN,
+        passAdmin: process.env.PASSADMIN,
+        githubClientId: process.env.GITHUB_CLIENT_ID,
+        githubClientSecret: process.env.GITHUB_CLIENT_SECRET
+    };
+};
---
+++ src/utils/logger.js
@@ -0,0 +1,50 @@
+import winston from 'winston';
+
+const customLevelOptions = {
+    levels: {
+        critical: 0,
+        error: 1,
+        warning: 2,
+        info: 3,
+        http: 4,
+        debug: 5
+    }
+}
+
+const devLogger = winston.createLogger({
+    levels: customLevelOptions.levels,
+    transports: [
+        new winston.transports.Console({
+            level: 'debug'
+        })
+    ]
+});
+
+const prodLogger = winston.createLogger({
+    levels: customLevelOptions.levels,
+    transports: [
+        new winston.transports.Console({
+            level: 'info'
+        }),
+        new winston.transports.File({
+            level: "error",
+            filename: "./prod-errors.log",
+            format: winston.format.simple()
+        })
+    ]
+});
+
+export const addLogger = (req, res, next) => {
+    switch (process.env.NODE_ENV) {
+        case 'development':
+            req.logger = devLogger;
+            break;
+        case 'production': 
+            req.logger = prodLogger;
+            break;
+        default:
+            throw new Error('invalid Environment')
+    }
+
+    next();
+};
