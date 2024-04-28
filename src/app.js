7ab7df08427b10940844d154dc07d4974ba354f4
SMartin <santiagomartin.44@gmail.com>
2024-04-28 17:35:57 -0300

    Documentar API

--- src/app.js
+++ src/app.js
@@ -1,43 +1,41 @@
+// Imports de Dependencias y demás
 // import express
 import express from 'express';
 // Importo handlebars
 import handlebars from 'express-handlebars';
-// Hago los imports de los Routers que se ubican en la carpeta routes
-import productsRoutes from './routes/products.routes.js';
-import cartsRoutes from './routes/carts.routes.js';
-import viewsRoutes from './routes/views.routes.js';
-import sessionRoutes from './routes/session.routes.js';
-// importo server para usa socket io
+// importo server para usar socket io
 import { Server } from 'socket.io';
-import { productModel } from './dao/models/products.model.js';
-/* import { Products } from './dao/ProductManager.js'; */
+// import de flash
+import flash from 'express-flash';
 //importo mongoose
 import mongoose from 'mongoose';
 import session from 'express-session';
 import MongoStore from 'connect-mongo';
 import cookieParser from 'cookie-parser';
-
 // Import de passport
 import passport from 'passport';
-import initializePassport from './config/passport.config.js';
+// import de swagger
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUIExpress from 'swagger-ui-express';
 
+// Imports de "Mis archivos"
+import initializePassport from './config/passport.config.js';
 // import port, secret, mongourl from config (dotENV)
 import { mongoUrl, port, secret } from './config/config.js'; 
-
-// import command
-
-
-// import ErrorHandler
-import { errorHandler } from './middlewares/error.js';
-
-// import mi middleware de winston logger "addLogger"
-import { addLogger } from './utils/logger.js';
+// Hago los imports de los Routers que se ubican en la carpeta routes
+import productsRoutes from './routes/products.routes.js';
+import cartsRoutes from './routes/carts.routes.js';
+import viewsRoutes from './routes/views.routes.js';
+import sessionRoutes from './routes/session.routes.js';
 import testRoutes from './routes/test.routes.js';
-// import de flash
-import flash from 'express-flash';
-
 import usersRoutes from './routes/users.routes.js'
 
+import { productModel } from './dao/models/products.model.js';
+// import ErrorHandler
+import { errorHandler } from './middlewares/error.js';
+// import mi middleware de winston logger "addLogger"
+import { addLogger } from './utils/logger.js';
+import { swaggerConfiguration } from './config/swagger-config.js';
 
 const app = express();
 app.use(express.json());
@@ -49,22 +47,17 @@ app.use(express.static('public'));
 app.use(addLogger)
 
 
-// uso command
-/* 
-const program = new Command ();
-program.option('--mode <mode>', 'Modo de trabajo', 'development');
-const options = program.parse();
-const { mongoUrl, port, secret } = getVariables(options); */
-
-
 // Declaro mi conexión con mongoose
 
-mongoose.connect(mongoUrl);
+const connectMongo = mongoose.connect(mongoUrl);
 
 const httpServer = app.listen(port, () => {
     console.log(`Server running on ${port}`);
 });
 
+// configuro swaggerconfiguration
+const specs = swaggerJSDoc(swaggerConfiguration)
+app.use('/apidocs', swaggerUIExpress.serve, swaggerUIExpress.setup(specs));
 
 // preparo websockets del lado del servidor 
 /* let productsData = new Products('./src/products.json'); */
---
+++ src/config/swagger-config.js
@@ -0,0 +1,12 @@
+export const swaggerConfiguration = {
+    definition: {
+        openapi: '3.0.1',
+        info: {
+            title: 'API e-commerce',
+            description: 'An API that makes the e-commerce work'
+        },
+    },
+    apis: ['src/docs/**/*.yaml']
+}
+
+/* import asd from '../docs/Products/' */
